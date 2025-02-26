c751d5fc57e4de3785c87acec86797bd050b6611
Nisit Jiraruwat <nisit.jiraruwat@gmail.com>
2025-02-26 21:12:42 +0700

    Add some comment

--- Calculator.cs
+++ Calculator.cs
@@ -1,5 +1,6 @@
 class Calculator
 {
+    /// Converts a string of digits into corresponding letters.
     public static string OldPhonePad(string input)
     {
       string result = "";
@@ -18,10 +19,12 @@ class Calculator
         if (char.IsDigit(c) && currentChar == c) {
           currentCharIndex++;
         } else {
+          // Add new alphabetical letter when previous character is number
           if (char.IsDigit(currentChar)) {
             result = $"{result}{GetOldPhonePadAlphabeticalLetter(currentChar, currentCharIndex)}";
           }
 
+          // Remove last text
           if (c == '*') {
             result = result[..^1];
           }
@@ -35,6 +38,7 @@ class Calculator
       return result;
     }
 
+    /// Maps a phone number digit to its corresponding alphabetical character.
     private static char GetOldPhonePadAlphabeticalLetter(char numberChar, int length)
     {
       string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -43,6 +47,7 @@ class Calculator
       return alphabet[alphabeticalIndex];
     }
 
+    /// Validates the input string for an old phone pad format (numbers[2-9], spaces, '*', and ending with '#').
     public static bool ValidateOldPhonePadInput(string input)
     {
       if (input.Length < 2 || input[^1] != '#' || !char.IsDigit(input[0])) {
